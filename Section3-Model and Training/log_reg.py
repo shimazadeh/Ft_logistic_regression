90765485e926455592d51e4b957700dd72936b0a
shimazadeh <zadeh.shima@gmail.com>
2023-11-15 18:58:24 -0500

    fixed issue-1

--- Section3-Model and Training/log_reg.py
+++ Section3-Model and Training/log_reg.py
@@ -2,13 +2,20 @@ import numpy as np
 import math
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+import matplotlib.pyplot as plt
+import joblib
+import json
+
+np.random.seed(42)
 
 class MyLogisticRegression():
-	def __init__(self, theta=None , alpha=0.001, max_iter=1000, label_encoder=None):
+	def __init__(self, theta=None , alpha=0.001, max_iter=100, batch=None, LB=LabelEncoder()):
 		self.alpha = alpha
 		self.max_iter = max_iter
 		self.theta = theta
-		self.label_encoder = LabelEncoder()
+		self.label_encoder = LB
+		self.losses = pd.DataFrame(columns=["Gryffindor", "Ravenclaw", "Slytherin", "Hufflepuff"])
+		self.batch = batch
 
 	def add_intercept(self, x):
 		if x is None:
@@ -29,37 +36,56 @@ class MyLogisticRegression():
 		predicted_labels = self.label_encoder.inverse_transform(y_encoded)
 		return (predicted_labels)
 
-	def sigmoid_(self, x):#for two features only
-		return (1 / (1 + np.exp(-x)))
+	def softmax_(self, x):
+		z = np.dot(x, self.theta)
+		exp_z = np.exp(z)
+		softmax = exp_z / np.sum(exp_z, axis=1, keepdims=True)
+		return (softmax)
 
-	def cost(self, x, y):
-		y_ = self.logistic_predict_(x)
-		m = len(x)
-		cost = (-1 / m) * (np.sum(y * np.log(y_)))
-		return (cost)
+	def loss_(self, y, y_hat):
+		y_hat = y_hat  + 1e-15  # to prevent log(0) or log(1)
+		loss = np.sum(y * np.log(y_hat) + (1.0 - y) * np.log(1.0 - y_hat), axis=0) / len(y)
+		return (-loss)
 	
+	def gradient(self, x, y, y_hat):
+		m = len(x)
+		gradient = np.dot(x.T, (y_hat - y)) / m 
+		return gradient
+		
 	def fit(self, x, y):
 		y_ = self.lab_encoder(y.squeeze())
-		m = len(x)
-		self.theta = np.random.randn(x.shape[1], y_.shape[1]) * 0.01
-		
-		# print("theta is:", self.theta)
-		for _ in range(self.max_iter):
-			h_ = self.probabilities(x)
-			grad= np.dot(x.T, (h_ - y_))/m
-			self.theta -= self.alpha * grad
-		# print("grad is:", grad)
-		# print("y encoded: ", y_)
+		self.theta = np.random.randn(x.shape[1], y_.shape[1]) * np.sqrt(2 / x.shape[1])
 
-	def probabilities(self, x):
-		res = np.dot(x, self.theta)
-		exp_x = np.exp(res)
-		sig_ = exp_x / np.sum(exp_x, axis=1, keepdims=True)#for two features: sig_ = sigmoid_(res)
-		probabilities = sig_
-		return probabilities
+		if self.batch is None:
+			self.batch = len(x)
+
+		for _ in range(self.max_iter):
+			indices = np.random.permutation(len(x))[:self.batch]
+
+			x_batch = x[indices]
+			y_batch = y_[indices]
+
+			h_ = self.softmax_(x_batch)
+			grad= self.gradient(x_batch, y_batch, h_)
+			self.theta -= self.alpha * grad
+			
+			loss = self.loss_(y_batch, h_)
+			self.losses.loc[len(self.losses)] = loss.flatten()
+			print(f"iteration {_}/{self.max_iter}: loss: {loss}")
+		self.visualize_training()
+		self.save_model()
+
+	def visualize_training(self):
+		for column_name, column_values in self.losses.items():
+			plt.plot(range(len(column_values)), column_values, label=column_name)
+		plt.xlabel('Iteration')
+		plt.ylabel('Loss')
+		plt.title(f'Loss per Class over Iterations: batch size {self.batch}')
+		plt.legend()
+		plt.savefig('Loss_training.png')
 
 	def predict(self, x):
-		probabilities = self.probabilities(x)
+		probabilities = self.softmax_(x)
 		y_encoded = np.argmax(probabilities, axis=1)
 		y_pred = self.label_decoder(y_encoded)
 		return (y_pred)
@@ -75,20 +101,18 @@ class MyLogisticRegression():
 	def confusion_matrix(self, y, y_hat, label=None):
 		unique_categories = sorted(pd.unique(y))
 		df = pd.DataFrame(index=unique_categories, columns=unique_categories)
-		print("total # of data: ", len(y))
+
 		for row_name, row_data in df.iterrows():
 			true_label = row_name
 			for column_name, column_data in row_data.items():
 				predicted_label = column_name
 				df.at[true_label, predicted_label] = self.count(y, y_hat, true_label, predicted_label)
 
-		print("the entire matrix:")
-		print(df)
 		if (label):
 			res = df.loc[label, label]
 		else:
 			res = df
-		print("the requested part:")
+		print("the confusion matrix part:")
 		print(res)
 		return (res)
 		
@@ -107,23 +131,28 @@ class MyLogisticRegression():
 		res = [TP, FN, FP, TN]
 		return (res)
 
-	def accuracy_score_(self, y, y_hat, label=None):
-		param = self.parameteres(y, y_hat,label)
-		accuracy =  (param[0] + param[3] / (param[0] + param[1] + param[2] + param[3]))
-		return (accuracy)
+	def performance(self, y, y_hat, label):
+		performance = {'accuracy': [], 'precision': [], 'recall': [], 'f1_score': []}
+		for l in label:
+			param = self.parameteres(y, y_hat, l)
+			
+			acc = (param[0] + param[3] / (param[0] + param[1] + param[2] + param[3]))
+			precision = 100 * (param[0] / (param[0] + param[2]))
+			recall = 100 * (param[0] / (param[0] + param[1]))
+			f1_score = (2 * precision * recall) / (1e-15 + precision + recall)
 
-	def precision_score_(self, y, y_hat, label=None):
-		param = self.parameteres(y, y_hat,label)
-		precision = param[0] / (param[0] + param[2])
-		return (precision)
+			performance['accuracy'].append(acc)
+			performance['precision'].append(precision)
+			performance['recall'].append(recall)
+			performance['f1_score'].append(f1_score)
 
-	def recall_score_(self, y, y_hat, label=None):
-		param = self.parameteres(y, y_hat, label)
-		recall = param[0] / (param[0] + param[1])
-		return (recall)
+		return performance
 
-	def F1_score_(self, y, y_hat, label=None):
-		precision = self.precision_score_(y, y_hat, label)
-		recall = self.recall_score_(y, y_hat, label)
-		F1 = (2 * precision * recall) / (precision + recall)
-		return (F1)
+	def save_model(self, filename='model.joblib'):
+		model_data = {'thetas': self.theta.tolist(), 'label_encoder': self.label_encoder}
+		joblib.dump(model_data, filename)
+
+	def load_model(self, file_name='model.joblib'):
+		model_data = joblib.load(file_name)
+		self.theta = model_data['thetas']
+		self.label_encoder = model_data['label_encoder']
---
+++ Section3-Model and Training/main.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from tabulate import tabulate
+from scipy.stats import skew, kurtosis
+import seaborn as sns
+from log_reg import MyLogisticRegression
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+import argparse
+import json
+import joblib
+import yaml
+import sys
+
+def training(lr, iteration, batch_size, file):
+    data_raw = pd.read_csv(file)
+    data = data_raw.dropna()
+    scaler = StandardScaler()
+
+    X_raw = data[["Divination", "Charms", "Flying", "Ancient Runes", "Defense Against the Dark Arts"]]
+    # X_raw = data[["Herbology", "Astronomy", "Ancient Runes", "Defense Against the Dark Arts"]]
+    y = data[["Hogwarts House"]]
+    X = scaler.fit_transform(X_raw)
+
+    #Step 1: clean up the data
+    logistic_regression = MyLogisticRegression(alpha=lr, max_iter=iteration, batch=batch_size)
+    X_ = logistic_regression.add_intercept(X)
+
+    # Step 2: Split the data 
+    X_train, X_test, y_train, y_test = train_test_split(X_, y, test_size=0.2, random_state=42)
+
+    # Step 3: Fit the model, visualize the training progress and saves the model
+    logistic_regression.fit(X_train, y_train)
+
+    # Step 4: Predict the class labels for your test data
+    y_pred = logistic_regression.predict(X_test)
+
+    # print("validation prediction: ", y_pred)
+    confusion_mat = logistic_regression.confusion_matrix(y_test.squeeze(), y_pred)
+
+    labels = ["Gryffindor", "Ravenclaw", "Slytherin", "Hufflepuff"]
+    performance = logistic_regression.performance(y_test.squeeze(), y_pred, labels)
+    
+    print(f"Category/performance  : precision  recall  accuracy f1_score")
+    print(f"{labels[0]}:            {(performance['precision'][0]): 0.2f}    {(performance['recall'][0]): 0.2f}  {(performance['accuracy'][0]): 0.2f}  {(performance['f1_score'][0]): 0.2f}")
+    print(f"{labels[1]}:             {(performance['precision'][1]): 0.2f}    {(performance['recall'][1]): 0.2f}  {(performance['accuracy'][1]): 0.2f}  {(performance['f1_score'][1]): 0.2f}")
+    print(f"{labels[2]}:             {(performance['precision'][2]): 0.2f}    {(performance['recall'][2]): 0.2f}  {(performance['accuracy'][2]): 0.2f}  {(performance['f1_score'][2]): 0.2f}")
+    print(f"{labels[3]}:            {(performance['precision'][3]): 0.2f}    {(performance['recall'][3]): 0.2f}  {(performance['accuracy'][3]): 0.2f}  {(performance['f1_score'][3]): 0.2f}")
+
+
+def testing(file, model_file):
+    data = pd.read_csv(file)
+    scaler = StandardScaler()
+
+    X_raw = data[["Herbology", "Astronomy", "Ancient Runes", "Defense Against the Dark Arts"]]
+    X = scaler.fit_transform(X_raw)
+
+    logistic_regression = MyLogisticRegression()
+    logistic_regression.load_model(model_file)
+
+    X_ = logistic_regression.add_intercept(X)
+
+    y_pred = logistic_regression.predict(X_)
+
+    with open('result.json', 'w') as json_file:
+        json.dump(y_pred.tolist(), json_file)
+    
+if __name__ == "__main__":
+    
+    with open(sys.argv[1], 'r') as file:
+        config = yaml.safe_load(file)
+    
+    lr = config['lr']
+    epoches = config['epoches']
+    batch_size = config['batch_size']
+    file = config['data_file']
+    model = config['model_file']
+    mode = config['mode']
+
+    if (mode == "train"):
+        training(lr, epoches, batch_size, file)
+    elif (mode == "test"):
+        if model is None:
+            print("model file is not provided")
+        testing(file, model)
+    else:
+        print("Mode is not provided")
